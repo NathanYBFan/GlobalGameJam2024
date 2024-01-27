a6e1a4aaaccf645575b4ee8bc92552d454763905
romank <xt19uj@brocku.ca>
2024-01-26 21:07:35 -0500

    Player Input + Controls
    - Added Player Movement along the X axis.
    - Added Jump (Want to refine it more though, it's really floaty)
    - Created workshop scene to avoid fucking up anything.

---
+++ BenguinBananaBallsTripleB/Assets/PlayerBody.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject body;
+	private PlayerController controller;
+	[Header("Parameters")]
+	[SerializeField]
+	private int jumpHeight = 100;
+	[SerializeField]
+	private int moveSpeed = 20;
+
+	private Rigidbody rb;
+	private bool isGrounded = true, isJumping = false, hasJumped = false, isDying = false, canMove = true, flip = false;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		controller = GameObject.Find("PlayerController").GetComponent<PlayerController>();
+		Debug.Assert(controller != null, "No controller on " + gameObject.name);
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateRotations();
+		if (controller.HasJumped && isGrounded) PerformJump();
+
+	}
+	private void FixedUpdate()
+	{
+		if (canMove)
+		{
+			Vector3 velocity = rb.velocity;
+			rb.velocity = (transform.right * controller.MovementDirection.x).normalized * moveSpeed;
+			rb.velocity = new Vector3(rb.velocity.x, velocity.y, 0);
+
+
+			RaycastHit hit;
+			Debug.DrawRay(transform.position + Vector3.up * 0.1f, Vector3.down * 0.2f);
+			if (Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out hit, 1f) && !isJumping)
+			{
+				isGrounded = true;
+				if (hasJumped)
+				{
+					hasJumped = false;
+				}
+			}
+			else isGrounded = false;
+		}
+
+	}
+	/// <summary>
+	/// This method makes the player jump (called in animationevent).
+	/// </summary>
+	private void PerformJump()
+	{
+		isJumping = true;
+		rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+		Invoke("ResetJump", 0.45f);
+	}
+	/// <summary>
+	/// This method reset the jump bools.
+	/// </summary>
+	private void ResetJump()
+	{
+		hasJumped = true;
+		isJumping = false;
+	}
+	/// <summary>
+	/// Method that updates the player's rotation (which direction they face).
+	/// </summary>
+	private void UpdateRotations()
+	{
+
+		if (controller.MovementDirection.x > 0 && canMove) flip = false;
+		else if (controller.MovementDirection.x < 0 && canMove) flip = true;
+
+		if (flip) body.transform.rotation = Quaternion.Euler(0, 180, 0);
+		else body.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+	}
+}
---
+++ BenguinBananaBallsTripleB/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Vector3 movementDirection; //A directional vector that maps WASD input to a Vector3 model.
+	public Vector3 MovementDirection { get { return movementDirection; } }
+	private bool hasJumped = false;
+	public bool HasJumped { get { return hasJumped; } }
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		CheckMovementVector();
+	}
+	/// <summary>
+	/// Helper method that checks movement inputs.
+	/// </summary>
+	private void CheckMovementVector()
+	{
+		movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+		if (Input.GetButtonDown("Jump")) hasJumped = true;
+		else hasJumped = false;
+	}
+}
