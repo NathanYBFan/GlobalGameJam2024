ef83c59a2755b8f5b28f6fe0e78eb03d55e34eab
Stephen <sgmerriam@proton.me>
2024-01-27 11:42:24 -0500

    Editor Tools
    - Notification.Display can send a notification to the unity scene view
    - Top bar to select scenes
    - Move components to top or bottom through right-click

---
+++ BenguinBananaBallsTripleB/Assets/Editor/SceneSelectionOverlay.cs
@@ -0,0 +1,71 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEditor.SceneManagement;
+using UnityEditor.Toolbars;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Editor.Editor
+{
+    [Overlay(typeof(SceneView), "Scene Selection")]
+    [Icon(kIcon)]
+    public class SceneSelectionOverlay : ToolbarOverlay
+    {
+        private const string kIcon = "Assets/Editor/Icons/UnityIcon.png";
+
+        private SceneSelectionOverlay() : base(SceneDropdownToggle.kId) { }
+
+        [EditorToolbarElement(kId, typeof(SceneView))]
+        internal class SceneDropdownToggle : EditorToolbarDropdownToggle, IAccessContainerWindow
+        {
+            public const string kId = "SceneSelectionOverlay/SceneDropdownToggle";
+
+            public EditorWindow containerWindow { get; set; }
+
+            private SceneDropdownToggle()
+            {
+                text = "Scenes";
+                tooltip = "Select a scene to load";
+                icon = AssetDatabase.LoadAssetAtPath<Texture2D>(kIcon);
+
+                dropdownClicked += ShowSceneMenu;
+            }
+
+            private void ShowSceneMenu()
+            {
+                GenericMenu menu = new();
+
+                Scene currentScene = EditorSceneManager.GetActiveScene();
+                
+                string[] sceneGuids = AssetDatabase.FindAssets("t:scene", null);
+
+                for (int i = 0; i < sceneGuids.Length; i++)
+                {
+                    string path = AssetDatabase.GUIDToAssetPath(sceneGuids[i]);
+
+                    string name = Path.GetFileNameWithoutExtension(path);
+                    
+                    menu.AddItem(new(name), string.Compare(currentScene.name, name) == 0, () => OpenScene(currentScene, path));
+                }
+                
+                menu.ShowAsContext();
+            }
+
+            private void OpenScene(Scene currentScene, string path)
+            {
+                if (currentScene.isDirty)
+                {
+                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        EditorSceneManager.OpenScene(path);
+                }
+                else
+                {
+                    EditorSceneManager.OpenScene(path);
+                }
+            }
+        }
+    }
+}
+#endif
