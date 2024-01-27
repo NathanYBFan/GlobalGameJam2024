954c2c954161247a5227bbd54b255014bc28d7e6
TimmyTheTempestarii <isholatimi56@gmail.com>
2024-01-27 12:15:12 -0500

    Updated QuickRestart

--- BenguinBananaBallsTripleB/Assets/Scripts/QuickRestart.cs
+++ BenguinBananaBallsTripleB/Assets/Scripts/QuickRestart.cs
@@ -5,16 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetButton("QuickRestart"))
         {
             resetTheGame();
         }
