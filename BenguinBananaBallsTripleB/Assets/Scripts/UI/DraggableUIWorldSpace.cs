7b0bd3647f32c59b6e7503efbbd2ad8fa77be44e
Nathan Fan <nf21dl@brocku.ca>
2024-01-28 10:16:05 -0500

    Prefab and level 2 fixes

--- BenguinBananaBallsTripleB/Assets/Scripts/UI/DraggableUIWorldSpace.cs
+++ BenguinBananaBallsTripleB/Assets/Scripts/UI/DraggableUIWorldSpace.cs
@@ -1,26 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DraggableUIWorldSpace : MonoBehaviour, IDragHandler, IBeginDragHandler
+public class DraggableUIWorldSpace : MonoBehaviour, IDragHandler
 {
     [SerializeField]
     private Canvas parentCanvasOfImageToMove;
     
     [SerializeField]
-    private Image imageToMove;
-
-    private Vector3 originalMouseClickPos;
-
+    private GameObject imageToMove;
 
     private void Start()
     {
         parentCanvasOfImageToMove.worldCamera = Camera.main;
-    }
-
-    public void OnBeginDrag(PointerEventData data)
-    {
-        originalMouseClickPos = data.position;
     }
 
     public void OnDrag(PointerEventData data)
